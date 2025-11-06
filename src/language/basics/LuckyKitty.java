fbda39f21dc82a8d4c86072b3f553761503df0a8
NaralaVeeralakshmi6 <NaralaVeeralakshmi6@outlook.com>
2025-11-06 20:54:16 +0530

    Commit

--- src/language/basics/LuckyKitty.java
+++ src/language/basics/LuckyKitty.java
@@ -4,7 +4,7 @@ public class LuckyKitty {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Lucky + Kitty");
+System.out.println("Lucky + Kitty + Chitti");
 	}
 
 }
