6cf6187218a60116fa1b22d58c33f5836f3f428d
NaralaVeeralakshmi6 <NaralaVeeralakshmi6@outlook.com>
2025-11-06 20:39:10 +0530

    add

---
+++ src/language/basics/LuckyKitty.java
@@ -0,0 +1,10 @@
+package language.basics;
+
+public class LuckyKitty {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Lucky + Kitty");
+	}
+
+}
